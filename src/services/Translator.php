213463276586b0de4131ba9f7e3c97cc80c27cc2
Prateek Rungta <prateekrungta@gmail.com>
2023-08-13 01:44:07 +0530

    Expose translate() method via translator service

--- src/services/Translator.php
+++ src/services/Translator.php
@@ -14,6 +14,7 @@ use yii\base\Component;
 class Translator extends Component
 {
     protected $_translator = null;
+    protected $_translatorOffline = false;
     
     public function getTranslator()
     {
@@ -28,5 +29,51 @@ class Translator extends Component
             }
         }
         return $this->_translator;
+    }
+    
+    public function translate($input, $targetLanguage, $sourceLanguage = '')
+    {
+        // fetch active translator
+        $translator = $this->getTranslator();
+        if (!$translator) {
+            Craft::debug("No translator found", __METHOD__);
+            return $input;
+        }
+        
+        if (!trim($input)) {
+            Craft::debug("Ignore empty string:$input", __METHOD__);
+            return $input;
+        }
+        
+        // remove region codes from Craft language locales
+        $targetLanguage = explode('-', $targetLanguage, 2)[0];
+        if ($sourceLanguage) {
+            $sourceLanguage = explode('-', $sourceLanguage, 2)[0];
+            Craft::debug("Translate from $sourceLanguage to $targetLanguage: $input", __METHOD__);
+        } else {
+            Craft::debug("Translate to $targetLanguage: $input", __METHOD__);
+        }
+
+        $cacheKey = [$translator::class, $input, $targetLanguage, $sourceLanguage];
+        $result = null;
+
+        // if the service is offline, query data cache only
+        if ($this->_translatorOffline) {
+            $result = Craft::$app->cache->get($cacheKey);
+        } else {
+            // otherwise, attempt both: data cache &
+            // translation service if cache miss
+            try {
+                $result = Craft::$app->cache->getOrSet(
+                    $cacheKey,
+                    fn() => $translator->translate($input, $targetLanguage, $sourceLanguage),
+                );
+            } catch (AutoTranslatorException $e) {
+                Craft::error("Translation service offline: $e", __METHOD__);
+                $this->_translatorOffline = true;
+            }
+        }
+        
+        return $result ?: $input;
     }
 }
